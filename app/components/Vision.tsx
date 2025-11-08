197b0007420e7be66d7cfae8ceaa401950561223
DavidFlautero <Davsarg33@gmail.com>
2025-11-07 22:23:01 -0300

    fondo vision

--- app/components/Vision.tsx
+++ app/components/Vision.tsx
@@ -43,7 +43,7 @@ export default function Vision() {
           poster="/videos/energia-neuro33-poster.jpg"
           className="w-full h-full object-cover"
         >
-          <source src="/videos/energia-neuro33.mp4" type="video/mp4" />
+          <source src="/videos/fondovision.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/40" /> {/* degradado sutil */}
       </div>
